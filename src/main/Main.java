d3e1ba03c5b91e6f319824c938d1190ef2919ae6
crispeuw360 <igornikolaidis@gmail.com>
2025-02-24 11:55:21 +0100

    2d

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import ventanas.VentanaIdentificate;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		VentanaIdentificate frame = new VentanaIdentificate();
+		frame.setVisible(true);
 	}
 
 }
